59165805a6b3cbf3bb8d02cff45f10c5ce1c0c05
Bartek Słowik <slowikbartek@gmail.com>
2017-03-04 19:51:57 +0100

    Changed methods in SimulationData.java class.

--- src/main/java/FCFS/simulationData/SimulationData.java
+++ src/main/java/FCFS/simulationData/SimulationData.java
@@ -2,22 +2,41 @@ package FCFS.simulationData;
 
 import process.Process;
 
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SimulationData {
 
-    private int numberOfProcesses;
+    private static int FROM = 5;
+    private static int TO = 500;
+
     private Queue<Process> processesFIFOQueue;
+    private int workingTime;
 
     public SimulationData(int numberOfProcesses) {
-        this.numberOfProcesses = numberOfProcesses;
-        processesFIFOQueue = new LinkedList<>();
+        processesFIFOQueue = new ConcurrentLinkedQueue<>();
+        for (int i = 0; i < numberOfProcesses; i++) {
+            addProcess();
+        }
     }
 
-    public void addProcess(int from, int to) {
-        processesFIFOQueue.add(new Process(from, to));
+    public void addProcess() {
+        Process process = new Process(getRandomPhaseProcessor(FROM, TO), workingTime);
+        processesFIFOQueue.add(process);
+
+        workingTime += process.getPhaseProcessor();
     }
 
+    public String showProcesses() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Process process : processesFIFOQueue) {
+            stringBuilder.append(process.toString() + "\n");
+        }
+        return stringBuilder.toString();
+    }
 
+    private static int getRandomPhaseProcessor(int from, int to) {
+        return ThreadLocalRandom.current().nextInt(from, to + 1);
+    }
 }
