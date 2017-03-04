416df88a159d7990305b306f2d0eabdb80e36377
Bartek Słowik <slowikbartek@gmail.com>
2017-03-04 21:24:43 +0100

    Added RR algorithm.

---
+++ src/main/java/RR/RR.java
@@ -0,0 +1,39 @@
+package RR;
+
+import process.Process;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static simulationData.Data.simulationDatas;
+
+public class RR {
+
+    private int timeQuantum;
+
+    public RR(int timeQuantum) {
+        this.timeQuantum = timeQuantum;
+    }
+
+    public int[] simulateRR() {
+        int averageAccess[] = new int[simulationDatas.length];
+        int nextProcessAccessTime = 0;
+        for (int i = 0; i < simulationDatas.length; i++) {
+            Queue<Process> processesFIFOQueue = new LinkedList<>(Arrays.asList(simulationDatas[i].getProcesses()));
+            for (int j = 0; j < processesFIFOQueue.size(); j++) {
+                Process process = processesFIFOQueue.poll();
+                if (process.getPhaseProcessor() < timeQuantum) {
+                    nextProcessAccessTime += process.getPhaseProcessor();
+                } else {
+                    process.setPhaseProcessor(process.getPhaseProcessor() - timeQuantum);
+                    nextProcessAccessTime += timeQuantum;
+                    processesFIFOQueue.add(process);
+                }
+            }
+            averageAccess[i] = nextProcessAccessTime / simulationDatas[i].getProcesses().length;
+            nextProcessAccessTime = 0;
+        }
+        return averageAccess;
+    }
+}
